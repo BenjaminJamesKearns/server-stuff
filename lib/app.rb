762fa431b9c1fd6bd7e7e597f21c80db4bd6a8e9
Ahmed <ahmed.marzook@cgi.com>
2018-10-16 10:57:58 +0000

    Added Html image

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,15 @@
 require "sinatra"
 
-get '/' do
-  "Hello World"
-end
+set :session_secret, 'super secret'
 
+get '/' do
+  "Hello World my name is mikeeeee"
+end
+get '/meme' do
+  "<div>
+  <img src='https://spectatorau.imgix.net/content/uploads/2017/08/Snip20170801_15.png?auto=compress,enhance,format&crop=faces,entropy,edges&fit=crop&w=820&h=550'>
+  </div>"
+end
 get '/secret' do
     "Hey BOIIII"
 end
