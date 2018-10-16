4d2ff02a3b9c961b1fdb2e93964a8d0262cafe58
Ahmed <ahmed.marzook@cgi.com>
2018-10-16 13:19:10 +0000

    Added a form

--- app.rb
+++ app.rb
@@ -6,8 +6,14 @@ get '/' do
   "Hello World my name is mikeeeee"
 end
 get '/meme' do
+  @name = ["jimmy", "boB","Sam","carl"].sample
   erb(:index)
 end
 get '/secret' do
     "Hey BOIIII"
+end
+
+get '/named-memes' do
+  @name = params[:name]
+  erb :index
 end
