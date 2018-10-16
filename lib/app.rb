c19613a91f9dbebfb5a04276114d3710322b1344
Ahmed <ahmed.marzook@cgi.com>
2018-10-16 11:00:14 +0000

    Added CSS

--- lib/app.rb
+++ lib/app.rb
@@ -6,7 +6,7 @@ get '/' do
   "Hello World my name is mikeeeee"
 end
 get '/meme' do
-  "<div>
+  "<div style='border: 3px dashed red'>
   <img src='https://spectatorau.imgix.net/content/uploads/2017/08/Snip20170801_15.png?auto=compress,enhance,format&crop=faces,entropy,edges&fit=crop&w=820&h=550'>
   </div>"
 end
